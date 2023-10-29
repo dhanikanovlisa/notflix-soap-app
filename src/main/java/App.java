30a238b096e9a2641c3c394adf40d0eb991f1d66
Rezapu Obed <obed.ubaidillah@gmail.com>
2023-10-29 16:10:38 +0700

    refactor(app): accepts all host

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,11 +4,11 @@ import service.SubscriptionWS;
 public class App {
     public static void main(String[] args) {
         try{
-            Endpoint.publish("http://soap-service:3030/ws/subscription", new SubscriptionWS());
+            Endpoint.publish("http://0.0.0.0:3030/ws/subscription", new SubscriptionWS());
             System.out.println("Start");
         }
         catch (Exception e){
-            System.out.println("Ooopsies");
+            e.printStackTrace();
         }
     }
 }
