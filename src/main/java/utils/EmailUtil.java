2d57292f651a44fe8cc069da65e91e6076029692
althaafka <althaafka004@gmail.com>
2023-11-15 11:15:45 +0700

    feat(email): add send email

---
+++ src/main/java/utils/EmailUtil.java
@@ -0,0 +1,67 @@
+package utils;
+
+import handler.ConfigHandler;
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class EmailUtil {
+	private static EmailUtil instance;
+
+	public static EmailUtil getInstance() {
+		if (instance == null) {
+			instance = new EmailUtil();
+		}
+		return instance;
+	}
+
+	// private String[] getAdminEmails(){
+		
+	// }
+
+	public void sendEmail(String to, String sub, String body){
+		try {
+			ConfigHandler ch = new ConfigHandler("config.email");
+
+			Properties props = System.getProperties();
+			props.put("mail.smtp.host", ch.get("mail.smtp.host"));
+			props.put("mail.smtp.port", ch.get("mail.smtp.port"));
+			props.put("mail.smtp.ssl.enable",ch.get("mail.smtp.ssl.enable"));
+			props.put("mail.smtp.auth", ch.get("mail.smtp.auth"));
+
+			Authenticator auth = authenticator(ch);
+			Session session = Session.getInstance(props, auth);
+
+			MimeMessage msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress(ch.get("mail.smtp.username")));
+			msg.setRecipients(Message.RecipientType.TO, to);
+			msg.setSubject(sub, "UTF-8");
+			msg.setText(body, "UTF-8");
+
+			System.out.println("Message is ready");
+    	  	Transport.send(msg);
+			System.out.println("EMail Sent Successfully!!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Authenticator authenticator(ConfigHandler ph) {
+		String username = ph.get("mail.smtp.username");
+		String password = ph.get("mail.smtp.password");
+		Authenticator auth = new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		};
+		return auth;
+	}
+
+	public static void main(String[] args) throws AddressException {
+		final String toEmail = "13521130@std.stei.itb.ac.id";
+		EmailUtil.getInstance().sendEmail(toEmail,"Subsription Notification", "Qwerty Uiopasdf Fghjkl Zxcvbn");
+	}
+}
