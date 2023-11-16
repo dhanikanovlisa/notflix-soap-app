8cd086f3eb61606143a6c8ec4a655b66200aa2ea
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-11-16 17:26:33 +0700

    fix editrequest

--- src/main/java/model/RequestFilmModel.java
+++ src/main/java/model/RequestFilmModel.java
@@ -1,4 +1,5 @@
 package model;
+
 import database.Database;
 import enums.Status;
 import lombok.AllArgsConstructor;
@@ -29,17 +30,17 @@ public class RequestFilmModel {
                 instance = new RequestFilmModel(new Database());
             }
             return instance;
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    /** Get All Request Film*/
+    /** Get All Request Film */
     public List<RequestFilm> getAllRequestFilm() throws SQLException {
         List<RequestFilm> listRequestFilm = new ArrayList<>();
         ResultSet rs = this.db.executeQuery("SELECT * FROM " + this.table + ";");
-        while(rs.next()){
+        while (rs.next()) {
             RequestFilm rf = new RequestFilm(rs);
             listRequestFilm.add(rf);
         }
@@ -47,7 +48,7 @@ public class RequestFilmModel {
         return listRequestFilm;
     }
 
-    /**Get all request film by id */
+    /** Get all request film by id */
     public List<RequestFilm> getAllRequestFilmById(int user_id) throws SQLException {
         List<RequestFilm> listRequestFilm = new ArrayList<>();
 
@@ -65,13 +66,13 @@ public class RequestFilmModel {
         return listRequestFilm;
     }
 
-    /**Get Request Film by Film Id */
-    public RequestFilm getRequestFilmByFilmId(int requestFilm_id) throws SQLException{
+    /** Get Request Film by Film Id */
+    public RequestFilm getRequestFilmByFilmId(int requestFilm_id) throws SQLException {
         RequestFilm rf = null;
         String query = "SELECT * FROM " + this.table + " WHERE requestFilm_id = ?";
         try (PreparedStatement pstmt = this.db.prepareStatement(query)) {
             this.db.bind(requestFilm_id);
-            
+
             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
                 rf = new RequestFilm(rs);
@@ -80,15 +81,14 @@ public class RequestFilmModel {
         return rf;
     }
 
-    /**Create Request Film*/
-    public String createRequestFilm( int user_id, String filmName, String description, String film_path,
-                                  String film_poster, String film_header, Date date_release, int duration){
+    /** Create Request Film */
+    public String createRequestFilm(int user_id, String filmName, String description, String film_path,
+            String film_poster, String film_header, Date date_release, int duration) {
 
         String query = "INSERT INTO " + this.table + " (user_id, filmName, description, film_path, " +
                 "film_poster, film_header, date_release, duration) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)";
         System.out.println(query);
-        try (PreparedStatement pstmt = this.db.prepareStatement(query))
-        {
+        try (PreparedStatement pstmt = this.db.prepareStatement(query)) {
             this.db.bind(user_id, filmName, description, film_path, film_poster, film_header, date_release, duration);
             int rowsAffected = pstmt.executeUpdate();
             return rowsAffected + " rows affected";
@@ -98,18 +98,19 @@ public class RequestFilmModel {
         }
     }
 
-    public String editRequestFilm(int requestFilm_id, int user_id, String filmName, String description, String film_path,
-                                  String film_poster, String film_header, Date date_release, int duration){
+    public String editRequestFilm(int requestFilm_id, int user_id, String filmName, String description,
+            String film_path, String film_poster, String film_header, Date date_release, int duration) {
+
         String query = "UPDATE " + this.table + " SET " +
                 "user_id = ?, filmName = ?, description = ?, film_path = ?, " +
                 "film_poster = ?, film_header = ?, date_release = ?, duration = ? " +
                 "WHERE requestFilm_id = ?";
 
         try (PreparedStatement pstmt = this.db.prepareStatement(query)) {
-            this.db.bind(requestFilm_id, user_id, filmName, description, film_path, film_poster, film_header, date_release, duration);
-
+            this.db.bind(user_id, filmName, description, film_path, film_poster, film_header, date_release, duration,
+                    requestFilm_id);
             int rowsAffected = pstmt.executeUpdate();
-            return rowsAffected + "rows affected";
+            return rowsAffected + " rows affected";
         } catch (SQLException e) {
             e.printStackTrace();
             return "";
